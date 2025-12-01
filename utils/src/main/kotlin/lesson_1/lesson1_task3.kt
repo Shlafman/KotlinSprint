4ab925220a4bd1867deff72de7785c394a52c515
crecorg <crecorg@gmail.com>
2025-12-02 00:49:14 +0200

    KS-1-3 do smth

--- utils/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ utils/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,16 @@
 package org.example.utils.lesson_1
 
 fun main() {
-    println("Test task 3")
+    val year: Int = 1961
+    var hour: Int  = 9
+    var minute: Int  = 7
+    println(year)
+    println(hour)
+    println(minute)
+    hour = 10
+    minute = 55
+    print(hour)
+    print(":")
+    print(minute)
+    //println("$hour:$minute")
 }
