f385c2d76ba138a9090c6dd968ef34f061482c78
crecorg <crecorg@gmail.com>
2025-12-02 01:49:00 +0200

    KS-1-4 do smth

---
+++ utils/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,10 @@
+package org.example.utils.lesson_1
+
+fun main()  {
+    val totalSeconds: Short = 6480
+    val hours: Int = totalSeconds / 3600
+    val minutes: Int  = (totalSeconds % 3600) / 60
+    val seconds: Int = totalSeconds % 60
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+}
