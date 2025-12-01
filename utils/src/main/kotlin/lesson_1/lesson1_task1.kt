711fc23ecb9532faa42b5df7bbdd9f6d3dd85370
crecorg <crecorg@gmail.com>
2025-12-02 00:28:30 +0200

    KS-1-2 do smth

--- utils/src/main/kotlin/lesson_1/lesson1_task1.kt
+++ utils/src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.utils.lesson_1
 
 fun main() {
     val numberOfOrders: Int = 75
-    val ThanksAnswer = "Thank you for order from us! "
+    val ThanksAnswer: String = "Thank you for order from us! "
 }
