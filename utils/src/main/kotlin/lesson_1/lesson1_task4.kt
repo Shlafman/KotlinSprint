d5bff171b8c1797200208d4598f2436c4a2c2724
crecorg <crecorg@gmail.com>
2025-12-02 01:13:40 +0200

    KS-1-4 do smth

---
+++ utils/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,20 @@
+package org.example.utils.lesson_1
+
+fun main() {
+    val length: Long = 40868600000L
+    val age: Byte = 27
+    val  partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("""
+        $length
+        $age
+        $partOfDay
+        $seconds
+        $partOfYear
+        $apogee
+    """.trimIndent())
+    //println("$length\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee")
+}
