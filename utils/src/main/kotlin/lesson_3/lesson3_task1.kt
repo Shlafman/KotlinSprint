9cf0f100b0ef4901c09235f8facb788e84c93458
crecorg <crecorg@gmail.com>
2025-12-03 00:54:35 +0200

    KS-3-1

---
+++ utils/src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.utils.lesson_3
+
+fun main() {
+    val userName = "Alex"
+    var welcome = "Good morning!"
+
+    println("$userName! $welcome")
+
+    welcome = "Good evening!"
+    println("$userName! $welcome")
+
+}
