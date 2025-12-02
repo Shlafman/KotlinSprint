2392480a98ff00442f66fc042b1040f0cd451266
crecorg <crecorg@gmail.com>
2025-12-02 21:30:40 +0200

    KS-1-4

--- utils/src/main/kotlin/lesson_1/lesson1_task4.kt
+++ utils/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -3,17 +3,19 @@ package org.example.utils.lesson_1
 fun main() {
     val length: Long = 40868600000L
     val age: Byte = 27
-    val  partOfDay: Float = 0.075F
+    val partOfDay: Float = 0.075F
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
-    println("""
+    println(
+        """
         $length
         $age
         $partOfDay
         $seconds
         $partOfYear
         $apogee
-    """.trimIndent())
+    """.trimIndent()
+    )
 }
