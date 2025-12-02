23f17b6f4402f342c1338ea983f3cc9fa9a341a5
crecorg <crecorg@gmail.com>
2025-12-02 18:38:10 +0200

    KS-2-1

--- utils/src/main/kotlin/lesson_1/lesson1_task1.kt
+++
@@ -1,7 +0,0 @@
-package org.example.utils.lesson_1
-
-fun main() {
-    val NumberOfOrders = 75
-    val ThanksAnswer = "Thank you for order from us! "
-
-}
--- utils/src/main/kotlin/lesson_1/lesson1_task2.kt
+++
@@ -1,5 +0,0 @@
-package org.example.utils.lesson_1
-
-fun main() {
-    println("Test task 2")
-}
