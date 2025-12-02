7f4e67f64018e9f88c0efb55bb8d9d049a3ed737
crecorg <crecorg@gmail.com>
2025-12-02 18:36:00 +0200

    KS-2-1

--- utils/src/main/kotlin/lesson_2/lesson2_task1.kt
+++ utils/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,10 +1,11 @@
 package org.example.utils.lesson_2
 
 fun main() {
-    val std1: Byte = 3 //std - Student
-    val std2: Byte = 4
-    val std3: Byte = 3
-    val std4: Byte = 5
-    val averageStd: Float = (std1 + std2 + std3 + std4) / 4f
-    println(averageStd)
+    val grade1 = 3
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+    val numberOfStudents = 4
+    val averageStd = ((grade1 + grade2 + grade3 + grade4) / numberOfStudents).toFloat()
+    println("%.2f".format(averageStd))
 }
