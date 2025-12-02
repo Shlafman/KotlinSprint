657351dd777239687b61a86f76b6d43f4cc64b56
crecorg <crecorg@gmail.com>
2025-12-02 19:20:03 +0200

    KS-2-1

---
+++ utils/src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,11 @@
+ackage org.example.utils.lesson_2
+
+fun main() {
+    val grade1 = 3
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+    val numberOfStudents = 4
+    val averageStd = ((grade1 + grade2 + grade3 + grade4) / numberOfStudents).toFloat()
+    println("%.2f".format(averageStd))
+}
