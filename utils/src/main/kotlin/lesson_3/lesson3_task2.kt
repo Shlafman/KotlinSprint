023594951bc44b714ce3e57c85f4dfc60725930f
crecorg <crecorg@gmail.com>
2025-12-03 01:08:17 +0200

    KS-3-2

---
+++ utils/src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.utils.lesson_3
+
+fun main() {
+    var years = 22
+    var fullName = "Sidorova Tatiana Sergeevna"
+
+    println("$fullName, $years")
+
+    years = 20
+    fullName = "Andreeva Tatiana Sergeevna"
+
+    println("$fullName, $years")
+}
