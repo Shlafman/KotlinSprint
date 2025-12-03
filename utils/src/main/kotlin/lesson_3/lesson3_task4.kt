98fae09564be26eda81deb0ba9d18922ff18a682
crecorg <crecorg@gmail.com>
2025-12-03 18:43:50 +0200

    KS-3-4

---
+++ utils/src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.utils.lesson_3
+
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+
+    var movement = "$from-$to:$moveNumber"
+    println(movement)
+
+    from = "D2"
+    to = "D3"
+    moveNumber++
+
+    movement = "$from-$to:$moveNumber"
+    println(movement)
+}
