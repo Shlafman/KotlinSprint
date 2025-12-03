f1efe49552666bd5e8fe98a704c4d4dd316ffdcf
crecorg <crecorg@gmail.com>
2025-12-03 15:28:59 +0200

    KS-2-5

--- utils/src/main/kotlin/lesson_2/lesson2_task5.kt
+++ utils/src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,11 +1,12 @@
 package org.example.utils.lesson_2
+import kotlin.math.pow
 
 fun main() {
     val investments = 70000
     val interestRate = 16.7
     val investmentPeriod = 20.0
+    val onehundred = 100.0
 
-
-    val finalAmount = investments * Math.pow(1 + interestRate / 100, investmentPeriod)
+    val finalAmount = investments * (1 + interestRate / onehundred).pow(investmentPeriod)
     println("%.3f".format(finalAmount))
 }
