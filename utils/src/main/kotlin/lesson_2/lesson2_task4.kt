348a1533f87326bb2a45529913c19eb724d4e629
crecorg <crecorg@gmail.com>
2025-12-03 15:09:58 +0200

    KS-2-4

--- utils/src/main/kotlin/lesson_2/lesson2_task4.kt
+++ utils/src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,11 +3,11 @@ package org.example.utils.lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buffProcent = 20
+    val buffProcent: Double = 20.0
     val oneHundredProcent = 100
 
-    val bonusCrystalOre = (crystalOre * buffProcent) / oneHundredProcent
-    val bonusIronOre = (ironOre * buffProcent) / oneHundredProcent
+    val bonusCrystalOre = (crystalOre * buffProcent) / oneHundredProcent.toInt()
+    val bonusIronOre = (ironOre * buffProcent) / oneHundredProcent.toInt()
 
     println("Crystalline ore: $bonusCrystalOre")
     println("Iron ore: $bonusIronOre")
