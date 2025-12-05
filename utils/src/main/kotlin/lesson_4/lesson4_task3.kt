1ca3b1c90e87be320dd5b3b4b11e4871d4a4f57c
crecorg <crecorg@gmail.com>
2025-12-05 17:55:14 +0200

    KS-4-3

--- utils/src/main/kotlin/lesson_4/lesson4_task3.kt
+++ utils/src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,11 +1,14 @@
 package org.example.utils.lesson_4
 
+const val REQUIRED_HUMIDITY = 20
+const val UNFAVORABLE_SEASON = "winter"
+
 fun main() {
-    val sunny: Boolean = true
-    val awningOpen: Boolean = true
+    val isSunny: Boolean = true
+    val isAwningOpen: Boolean = true
     val humidity: Int = 20
     val currentSeason: String = "winter"
 
-    val conditions = sunny && awningOpen && (humidity == 20) && (currentSeason != "winter")
+    val conditions = isSunny && isAwningOpen && (humidity == REQUIRED_HUMIDITY) && (currentSeason != UNFAVORABLE_SEASON)
     println("Are the conditions now favorable for the growth of legumes? $conditions")
 }
