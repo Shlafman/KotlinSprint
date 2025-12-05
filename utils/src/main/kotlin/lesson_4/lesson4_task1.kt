636e709f013aee107a48b522840376330e8ada4d
crecorg <crecorg@gmail.com>
2025-12-05 17:34:45 +0200

    KS-4-1

--- utils/src/main/kotlin/lesson_4/lesson4_task1.kt
+++ utils/src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,9 +1,8 @@
 package org.example.utils.lesson_4
-
+const val TOTAL_TABLES = 13
 fun main() {
-    val allTables = 13
     val reservedToday = 13
     val reservedTomorrow = 9
 
-    println("Today free tables: ${reservedToday < allTables}\nFree tables for tomorrow: ${reservedTomorrow < allTables}")
+    println("Today free tables: ${reservedToday < TOTAL_TABLES}\nFree tables for tomorrow: ${reservedTomorrow < TOTAL_TABLES}")
 }
