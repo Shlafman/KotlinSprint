3e24f21badb476f6717e100dcd2fd266d218469c
crecorg <crecorg@gmail.com>
2025-12-05 17:45:10 +0200

    KS-4-2

--- utils/src/main/kotlin/lesson_4/lesson4_task2.kt
+++ utils/src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,8 +1,9 @@
 package org.example.utils.lesson_4
 
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+
 fun main() {
-    val minWeight = 35
-    val maxWeight = 100
     val maxVolume = 100
 
     val weight1 = 20
@@ -13,10 +14,10 @@ fun main() {
 
     println(
         "Load weight $weight1 kg and volume $volume1 L matches the category 'Average': " +
-                "${(weight1 > minWeight && weight1 <= maxWeight) && (volume1 < maxVolume)}"
+                "${(weight1 > MIN_WEIGHT && weight1 <= MAX_WEIGHT) && (volume1 < maxVolume)}"
     )
     println(
         "Load weight $weight2 kg and volume $volume2 L matches the category 'Average': " +
-                "${(weight2 > minWeight && weight2 <= maxWeight) && (volume2 < maxVolume)}"
+                "${(weight2 > MIN_WEIGHT && weight2 <= MAX_WEIGHT) && (volume2 < maxVolume)}"
     )
 }
